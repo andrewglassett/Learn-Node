6b22f93bb07bdfc1cea61000b34977f361101570
Andrew Glassett <aglassett@fhkpww1.theorchard.local>
2017-05-10 11:58:37 -0400

    add store controller

--- dang-that-is-delicious/routes/index.js
+++ dang-that-is-delicious/routes/index.js
@@ -1,23 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController');
 
-// Do work here
-router.get('/', (req, res) => {
-  const drew = { name: 'Wes', age: 100, cool: true};
-  // res.send('Hey! It works!');
-  // res.json(drew);
-  // res.send(req.query.name);
-  // res.json(req.query);
-  res.render('hello', {
-    name: 'wes',
-    dog: req.query.dog,
-    title: 'heyo'
-  });
-});
+const { catchErrors } = require('../handlers/errorHandlers');
 
-router.get('/reverse/:name', (req, res) => {
-  const reverse = [...req.params.name].reverse().join('');
-  res.send(reverse);
-})
+router.get('/',  storeController.homePage);
+router.get('/add',  storeController.addStore);
+router.post('/add', catchErrors(storeController.createStore));
 
 module.exports = router;
